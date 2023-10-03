f15bcecc43f26270116f22df59ace2a5b60d7174
KirillDemyanenko <demyanenkokg@gmail.com>
2023-10-03 18:53:43 +0300

    refactor: add more info in card

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,15 +1,38 @@
 import React from 'react';
-import { ItemProps, State } from '../types';
-
-export default class Item extends React.Component<ItemProps, State> {
+import { ItemProps, ItemState, PokemonInfo } from '../types';
+import image from '../assets/load.gif';
+export default class Item extends React.Component<ItemProps, ItemState> {
+  info: PokemonInfo = {} as PokemonInfo;
+  image = '';
   constructor(props: ItemProps) {
     super(props);
+    this.state = { isLoad: false, imgURL: '' };
+  }
+
+  async componentDidMount() {
+    this.info = await fetch(this.props.pokemonInfo.url).then((data) => data.json());
+    console.log(this.info);
+    this.setState({ isLoad: true, imgURL: this.info.sprites.front_default as string });
   }
 
   render() {
     return (
       <div className="res" key={this.props.key}>
         <h4>{this.props.pokemonInfo.name}</h4>
+        <img src={this.state.isLoad ? this.state.imgURL : image} alt="img" />
+        <p>Height: {this.info.height || 'Loading...'}</p>
+        <p>Weight: {this.info.weight || 'Loading...'}</p>
+        <div className="stats">
+          <h5>Stats</h5>
+          {this.info?.stats?.map((stat) => {
+            return (
+              <div className={'stats-row'} key={`stat-${stat.stat.name}`}>
+                <p className={'stats-info'}>{stat.stat?.name || 'Loading'}</p>
+                <p className={'stats-info'}>{stat?.base_stat || 'Loading'}</p>
+              </div>
+            );
+          })}
+        </div>
         <a href={this.props.pokemonInfo.url}>more details</a>
       </div>
     );
