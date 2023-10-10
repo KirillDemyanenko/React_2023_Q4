33104b614d00d21144f178a63493695d3a741cea
KirillDemyanenko <demyanenkokg@gmail.com>
2023-10-10 13:49:52 +0300

    feat: add assets

---
+++ src/images.ts
@@ -0,0 +1,40 @@
+import bug from './assets/icons/bug.svg';
+import dark from './assets/icons/dark.svg';
+import dragon from './assets/icons/dragon.svg';
+import electric from './assets/icons/electric.svg';
+import fairy from './assets/icons/fairy.svg';
+import fighting from './assets/icons/fighting.svg';
+import fire from './assets/icons/fire.svg';
+import flying from './assets/icons/flying.svg';
+import ghost from './assets/icons/ghost.svg';
+import grass from './assets/icons/grass.svg';
+import ground from './assets/icons/ground.svg';
+import ice from './assets/icons/ice.svg';
+import normal from './assets/icons/normal.svg';
+import poison from './assets/icons/poison.svg';
+import psychic from './assets/icons/psychic.svg';
+import rock from './assets/icons/rock.svg';
+import steel from './assets/icons/steel.svg';
+import water from './assets/icons/water.svg';
+import { TypesImages } from './types';
+
+export const images: TypesImages = {
+  bug: bug,
+  electric: electric,
+  water: water,
+  dragon: dragon,
+  fairy: fairy,
+  fighting: fighting,
+  fire: fire,
+  flying: flying,
+  ghost: ghost,
+  grass: grass,
+  ground: ground,
+  ice: ice,
+  normal: normal,
+  dark: dark,
+  poison: poison,
+  psychic: psychic,
+  rock: rock,
+  steel: steel,
+};
