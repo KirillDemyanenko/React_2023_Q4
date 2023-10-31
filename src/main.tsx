86566aff82362349aa1a1d2656570abd1e13c28b
KirillDemyanenko <demyanenkokg@gmail.com>
2023-10-31 10:08:00 +0300

    feat: add router

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import ErrorPage from './pages/ErrorPage/error-page';
 
 window.addEventListener('error', (ev) => {
   ev.preventDefault();
   console.error('Render error ', ev.error.message);
 });
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
